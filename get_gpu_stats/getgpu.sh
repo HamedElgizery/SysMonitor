85093b070eebbbcf601793b9dad3d4d4e5df6fbe
waryoyo <youssefhus2005@hotmail.com>
2024-12-19 01:14:01 +0200

    mem usage working !

--- get_gpu_stats/getgpu.sh
+++ get_gpu_stats/getgpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- ./process_gpu.py "$(nvidia-smi --query-gpu=gpu_name,utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,fan.speed,temperature.gpu,memory.total,memory.free,memory.used,power.draw,power.max_limit,clocks.sm,clocks.mem,clocks.gr,clocks.video --format=csv)" > output.txt
+./process_gpu.py "$(nvidia-smi --query-gpu=gpu_name,utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,fan.speed,temperature.gpu,memory.total,memory.free,memory.used,power.draw,power.max_limit,clocks.sm,clocks.mem,clocks.gr,clocks.video --format=csv)" > output.txt
