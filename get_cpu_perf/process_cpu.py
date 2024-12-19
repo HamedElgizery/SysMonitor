e00c16819ca0208a7e494fa05b55f98029e09459
Hamed Elgizery <hamedashraf2004@gmail.com>
2024-12-19 13:01:29 +0200

    Remove CPU All field

--- get_cpu_perf/process_cpu.py
+++ get_cpu_perf/process_cpu.py
@@ -4,7 +4,8 @@ from sys import argv
 
 mpstat_data = json.loads(argv[1])
 
-
 cpu_stats = mpstat_data['sysstat']['hosts'][0]['statistics'][0]['cpu-load'][0]
 for key, value in cpu_stats.items():
-    print(key, value)
+    if key == "cpu":
+        continue
+    print(key.upper(), value)
