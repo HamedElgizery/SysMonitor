36db9bf0cd24e1d6a0c9a6b8021e426308796c87
waryoyo <youssefhus2005@hotmail.com>
2024-12-21 22:38:53 +0200

    dockerfile logging

---
+++ logger.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOG_DIR="/scripts"
+
+while true; do
+  for dir in log*/; do
+      if [ -d "$dir" ]; then
+        for script in "$dir"*.sh; do
+            if [ -f "$script" ]; then
+                bash "$script"
+            fi
+        done
+      fi
+    done
+  sleep 10
+done
+
