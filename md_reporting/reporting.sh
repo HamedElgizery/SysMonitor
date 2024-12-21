7e876f0181b4ab2879f9a5b1dc99e4da7fd5dc83
Hamed Elgizery <hamedashraf2004@gmail.com>
2024-12-22 01:16:49 +0200

    Add docker instructions

--- md_reporting/reporting.sh
+++ md_reporting/reporting.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-timestamp=$(date +%s).md
-./generate_report.py $timestamp
+LOG_DIR="/scripts"
+
+while true; do
+  for dir in log*/; do
+      if [ -d "$dir" ]; then
+        for script in "$dir"*.sh; do
+            if [ -f "$script" ]; then
+                timestamp=$(date +%s).md
+                ./generate_report.py $timestamp
+            fi
+        done
+      fi
+    done
+  sleep 10
+done
+
+
