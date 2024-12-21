27b35f4c77d00458f625c1aa5061d6fabc80f8a8
waryoyo <youssefhus2005@hotmail.com>
2024-12-21 21:45:10 +0200

    logging for more stuff !

--- get_gpu_stats/loggpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$0")"
-
-nvidia_output=$(nvidia-smi --query-gpu=gpu_name,utilization.gpu,utilization.memory,utilization.encoder,utilization.decoder,fan.speed,temperature.gpu,memory.total,memory.free,memory.used,power.draw,power.max_limit,clocks.sm,clocks.mem,clocks.gr,clocks.video --format=csv)
-
-$SCRIPT_DIR/process_gpu_logging.py "$nvidia_output" >> logoutput.txt
