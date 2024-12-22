8909758ca88002e58707a3d7cf237a28bb7b97fe
Hamed Elgizery <hamedashraf2004@gmail.com>
2024-12-22 05:29:37 +0200

    Remove trash

--- md_reporting/startdocker.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-sudo docker stop reporting
-sudo docker image build -f dockerfile.report -t report-image .
-sudo docker run --rm  --privileged --name reporting --mount type=bind,src=/home/hamed/src/SysMonitor/logging,target=/logs,readonly --mount type=bind,src=/home/hamed/src/SysMonitor/md_reporting/reports,target=/reports report-image
-
